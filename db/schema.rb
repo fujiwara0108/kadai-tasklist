00517f6af4b9208d103733c1756e9bc433c2cefa
motoki.fujiwara <h0108bfddrr@gmail.com>
2020-07-09 12:23:52 +0000

    validdation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_090050) do
+ActiveRecord::Schema.define(version: 2020_07_09_121546) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
+    t.string "status"
   end
 
 end
